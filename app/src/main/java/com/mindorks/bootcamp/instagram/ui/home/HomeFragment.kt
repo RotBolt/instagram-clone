3dcf1c969806baf0bba7cd40c89f0ab97997279d
Rohan Maity <rmrohan03@gmail.com>
2020-04-16 22:28:54 +0530

    add home/posts screen

--- app/src/main/java/com/mindorks/bootcamp/instagram/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/home/HomeFragment.kt
@@ -2,9 +2,15 @@ package com.mindorks.bootcamp.instagram.ui.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mindorks.bootcamp.instagram.R
 import com.mindorks.bootcamp.instagram.di.component.FragmentComponent
 import com.mindorks.bootcamp.instagram.ui.base.BaseFragment
+import com.mindorks.bootcamp.instagram.ui.home.posts.PostAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
+import javax.inject.Inject
 
 class HomeFragment : BaseFragment<HomeViewModel>(){
 
@@ -19,14 +25,46 @@ class HomeFragment : BaseFragment<HomeViewModel>(){
         }
     }
 
+    @Inject
+    lateinit var linearLayoutManager: LinearLayoutManager
+
+    @Inject
+    lateinit var postAdapter: PostAdapter
+
     override fun provideLayoutId(): Int = R.layout.fragment_home
 
     override fun injectDependencies(fragmentComponent: FragmentComponent) {
         fragmentComponent.inject(this)
     }
 
-    override fun setupView(view: View) {
+    override fun setupObservers() {
+        super.setupObservers()
 
+        viewModel.posts.observe(this, Observer {
+            it.data?.run{ postAdapter.appendData(this) }
+        })
+
+        viewModel.loading.observe(this, Observer {
+            progressBar.visibility = if(it) View.VISIBLE else View.GONE
+        })
     }
 
+    override fun setupView(view: View) {
+        rvPosts.apply {
+            layoutManager = linearLayoutManager
+            adapter = postAdapter
+            addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    layoutManager?.run {
+                        if (this is LinearLayoutManager
+                            && itemCount > 0
+                            && itemCount == findLastCompletelyVisibleItemPosition() + 1){
+                            viewModel.onLoadMore()
+                        }
+                    }
+                }
+            })
+        }
+    }
 }
--- app/src/main/java/com/mindorks/bootcamp/instagram/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/home/HomeViewModel.kt
@@ -1,16 +1,68 @@
 package com.mindorks.bootcamp.instagram.ui.home
 
+import androidx.lifecycle.MutableLiveData
+import com.mindorks.bootcamp.instagram.data.model.Post
+import com.mindorks.bootcamp.instagram.data.repository.PostRepository
+import com.mindorks.bootcamp.instagram.data.repository.UserRepository
 import com.mindorks.bootcamp.instagram.ui.base.BaseViewModel
+import com.mindorks.bootcamp.instagram.utils.common.Resource
 import com.mindorks.bootcamp.instagram.utils.network.NetworkHelper
 import com.mindorks.bootcamp.instagram.utils.rx.SchedulerProvider
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.processors.PublishProcessor
 
 class HomeViewModel(
     schedulerProvider: SchedulerProvider,
     compositeDisposable: CompositeDisposable,
-    networkHelper: NetworkHelper
+    networkHelper: NetworkHelper,
+    private val userRepository: UserRepository,
+    private val postRepository: PostRepository,
+    private val allPosts : ArrayList<Post>,
+    private val paginator:PublishProcessor<Pair<String?,String?>>
 ):BaseViewModel(schedulerProvider, compositeDisposable, networkHelper){
-    override fun onCreate() {
 
+    val loading = MutableLiveData<Boolean>()
+    val posts = MutableLiveData<Resource<List<Post>>>()
+
+    private val user = userRepository.getCurrentUser()!!
+
+    init {
+        compositeDisposable.add(
+            paginator
+                .onBackpressureDrop()
+                .doOnNext{
+                    loading.postValue(true)
+                }
+                .concatMapSingle { pageIds ->
+                    return@concatMapSingle postRepository.fetchHomePostList(pageIds.first, pageIds.second, user)
+                        .subscribeOn(schedulerProvider.io())
+                        .doOnError {
+                            handleNetworkError(it)
+                        }
+                }
+                .subscribe(
+                    {
+                        allPosts.addAll(it)
+                        loading.postValue(false)
+                        posts.postValue(Resource.success(it))
+                    },{
+                        handleNetworkError(it)
+                    }
+                )
+        )
+    }
+    override fun onCreate() {
+        loadMorePosts()
+    }
+
+    private fun loadMorePosts(){
+        val firstPostId = if(allPosts.isNotEmpty()) allPosts[0].id else null
+        val lastPostId = if(allPosts.size > 1) allPosts[allPosts.size -1].id else null
+        if(checkInternetConnectionWithMessage()) paginator.onNext(Pair(firstPostId,lastPostId))
+    }
+
+
+    fun onLoadMore(){
+        if(loading.value !== null && loading.value == false) loadMorePosts()
     }
 }
